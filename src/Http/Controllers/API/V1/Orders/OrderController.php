adbc16686c60a399f0649e15caf635b50139cc8b
SajaD SaeeDi <thunder11like@gmail.com>
2024-02-08 08:19:49 +0330

    [Add] Order controller

---
+++ src/Http/Controllers/API/V1/Orders/OrderController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sajadsdi\Marketplace\Http\Controllers\API\V1\Orders;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Sajadsdi\Marketplace\Http\Controllers\API\V1\BaseApiController;
+use Illuminate\Http\Request;
+
+class OrderController extends BaseApiController
+{
+    use HasFactory;
+}
