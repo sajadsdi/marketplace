a971a17e66e98492d5c4a0d6e683a6e57052858c
SajaD SaeeDi <thunder11like@gmail.com>
2024-02-08 08:01:38 +0330

    [Add] marketplace config

---
+++ config/marketplace.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'models' => [
+        'user'          => \Sajadsdi\Marketplace\Model\User\User::class,
+        'product'       => \Sajadsdi\Marketplace\Model\Product\Product::class,
+        'product_photo' => \Sajadsdi\Marketplace\Model\Products\ProductPhoto::class,
+        'order'         => \Sajadsdi\Marketplace\Model\Order\Order::class
+    ]
+];
