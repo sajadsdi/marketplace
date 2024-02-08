92937478e512392964e8f5534a37142263e60725
SajaD SaeeDi <thunder11like@gmail.com>
2024-02-08 08:20:31 +0330

    [Add] Product controller

---
+++ src/Http/Controllers/API/V1/Products/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sajadsdi\Marketplace\Http\Controllers\API\V1\Products;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Sajadsdi\Marketplace\Http\Controllers\API\V1\BaseApiController;
+use Illuminate\Http\Request;
+
+class ProductController extends BaseApiController
+{
+    use HasFactory;
+}
