dfce7956cd372d30ab4491d2e9a28c6b615040c8
SajaD SaeeDi <thunder11like@gmail.com>
2024-02-08 08:21:12 +0330

    [Add] ProductPhoto controller

---
+++ src/Http/Controllers/API/V1/Products/ProductPhotoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sajadsdi\Marketplace\Http\Controllers\API\V1\Products;
+
+use Sajadsdi\Marketplace\Http\Controllers\API\V1\BaseApiController;
+use Illuminate\Http\Request;
+
+class ProductPhotoController extends BaseApiController
+{
+    //
+}
