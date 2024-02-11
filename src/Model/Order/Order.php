aa1d09deed5ce9315269feaa40c744389b7587af
SajaD SaeeDi <thunder11like@gmail.com>
2024-02-11 22:57:51 +0330

    [Update] order model

--- src/Model/Order/Order.php
+++ src/Model/Order/Order.php
@@ -4,8 +4,11 @@ namespace Sajadsdi\Marketplace\Model\Order;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Sajadsdi\Marketplace\Enums\Order\OrderStatus;
+use Sajadsdi\Marketplace\Jobs\SendOrderSubmittedEmail;
 use Sajadsdi\Marketplace\Model\Product\Product;
 use Sajadsdi\Marketplace\Model\User\User;
 
@@ -13,22 +16,40 @@ class Order extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['user_id', 'total_price', 'status'];
+    protected $fillable = ['user_id', 'total_price', 'status', 'shipping'];
 
     protected $casts = [
-        'status' => OrderStatus::class
+        'status'   => OrderStatus::class,
     ];
 
-    public function user()
+    /**
+     * relation to user model
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-
-    public function products()
+    /**
+     * relation to product model
+     *
+     * @return BelongsToMany
+     */
+    public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class, 'order_product')
-            ->withPivot('quantity', 'price', 'shipping_price','total_price')
+            ->withPivot('quantity', 'price', 'shipping_price')
             ->withTimestamps();
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($order) {
+            SendOrderSubmittedEmail::dispatch($order->id);
+        });
     }
 }
