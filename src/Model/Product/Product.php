3f12649588c3a3ac14018264bc6fb6df9a0e6502
SajaD SaeeDi <thunder11like@gmail.com>
2024-02-11 22:59:07 +0330

    [Update] product model

--- src/Model/Product/Product.php
+++ src/Model/Product/Product.php
@@ -4,9 +4,11 @@ namespace Sajadsdi\Marketplace\Model\Product;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Sajadsdi\Marketplace\Model\Order\Order;
-use Sajadsdi\Marketplace\Model\Products\ProductPhoto;
 use Sajadsdi\Marketplace\Model\User\User;
 
 class Product extends Model
@@ -15,20 +17,53 @@ class Product extends Model
 
     protected $fillable = ['user_id', 'title', 'price', 'shipping_price','total_price'];
 
-    public function user()
+    /**
+     * relation to user model
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function orders()
+    /**
+     * relation to order model
+     *
+     * @return BelongsToMany
+     */
+    public function orders(): BelongsToMany
     {
         return $this->belongsToMany(Order::class, 'order_product')
             ->withPivot('quantity', 'price', 'shipping_price','total_price')
             ->withTimestamps();
     }
 
-    public function photos()
+    /**
+     * relation to product photo model
+     *
+     * @return HasMany
+     */
+    public function photos(): HasMany
     {
         return $this->hasMany(ProductPhoto::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($product) {
+            $product->calculateTotalPrice();
+        });
+
+        static::updating(function ($product) {
+            $product->calculateTotalPrice();
+        });
+    }
+
+    private function calculateTotalPrice()
+    {
+        $this->total_price = $this->price + $this->shipping_price;
     }
 }
