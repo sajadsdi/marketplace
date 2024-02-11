2c175f895b7ce8e4c57e5b732ad75285d24ebacd
SajaD SaeeDi <thunder11like@gmail.com>
2024-02-11 23:05:57 +0330

    [Add] mailable

---
+++ src/Mail/OrderSubmittedEmail.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Sajadsdi\Marketplace\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class OrderSubmittedEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public $orderDetails)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Order Submitted Email',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'emails.orders.submitted',
+            with: [
+                'order' => $this->orderDetails,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
